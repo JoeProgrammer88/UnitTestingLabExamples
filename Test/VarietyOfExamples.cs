1136e1bb34b1ea466464ad94cbb77f4d71131206
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-02-05 13:44:44 -0800

    Refactor IsLeapYear to make it better than Joe's version

--- Test/VarietyOfExamples.cs
+++ Test/VarietyOfExamples.cs
@@ -15,7 +15,7 @@ public class VarietyOfExamples
     /// <returns></returns>
     public bool IsLeapYear(int year)
     {
-        return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+        return year % 4 == 0 && year % 100 != 0;
     }
 }
 
